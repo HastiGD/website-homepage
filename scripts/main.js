f8c8a6e6e3f0903bb0c46fe7b47b3ca35d6c9e2b
Hasti Gheibi Dehnashi <hastidehnashi@gmail.com>
2021-02-14 20:33:13 -0800

    added email verification

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+function ValidateEmail() {
+	var email = document.getElementById("emailInput");
+	var message = document.getElementById("messageInput");
+	var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	if (email.value.match(mailformat) && message.value) {
+		email.value = "";
+		message.value = "";
+		alert("Message sent!");
+		// document.form1.text1.focus();
+		return true;
+	}
+	if (!email.value.match(mailformat)) {
+		alert("Invalid email");
+		email.focus();
+		return false;
+	}
+	if (!message.value) {
+		alert("Please enter a message!");
+		message.focus();
+		return false;
+	}
+}
